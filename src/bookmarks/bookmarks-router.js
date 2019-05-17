4b0e4d27a452cf4545a97086bf9cedc755364924
Kyler <Krenneker16@gmail.com>
2019-05-16 22:48:03 -0400

    fixed errors associated with patch method and patch tests

--- src/bookmarks/bookmarks-router.js
+++ src/bookmarks/bookmarks-router.js
@@ -1,28 +1,26 @@
-const express = require('express');
-const xss = require('xss');
-const logger = require('../logger');
-const { bookmarks } = require('../store');
-const BookmarksService = require('./bookmarks-service');
+const express = require("express");
+const xss = require("xss");
+const logger = require("../logger");
+const { bookmarks } = require("../store");
+const BookmarksService = require("./bookmarks-service");
 
 const bookmarkRouter = express.Router();
 const bodyParser = express.json();
 
-const serializeBookmark = bookmark => (
-  {
-    id: bookmark.id,
-    title: xss(bookmark.title),
-    description: xss(bookmark.description),
-    url: bookmark.url,
-    rating: bookmark.rating
-  }
-)
+const serializeBookmark = bookmark => ({
+  id: bookmark.id,
+  title: xss(bookmark.title),
+  description: xss(bookmark.description),
+  url: bookmark.url,
+  rating: bookmark.rating
+});
 bookmarkRouter
-  .route('/')
+  .route("/")
   .get((req, res, next) => {
-    const knexInstance = req.app.get('db');
+    const knexInstance = req.app.get("db");
     BookmarksService.getAllBookmarks(knexInstance)
       .then(bookmarks => {
-        res.json(bookmarks.map(serializeBookmark))
+        res.json(bookmarks.map(serializeBookmark));
       })
       .catch(next);
   })
@@ -32,19 +30,25 @@ bookmarkRouter
     const newBookmark = { title, url, description, rating };
 
     if (!title) {
-      logger.error('Title is required');
-      return res.status(400).json({ error: { message: `Missing 'title' in request body` } });
+      logger.error("Title is required");
+      return res
+        .status(400)
+        .json({ error: { message: `Missing 'title' in request body` } });
     }
     if (!url) {
       logger.error(`Url is required`);
-      return res.status(400).json({ error: { message: `Missing 'url' in request body` } });
+      return res
+        .status(400)
+        .json({ error: { message: `Missing 'url' in request body` } });
     }
     if (!rating) {
       logger.error(`Rating is required`);
-      return res.status(400).json({ error: { message: `Missing 'rating' in request body` } });
+      return res
+        .status(400)
+        .json({ error: { message: `Missing 'rating' in request body` } });
     }
 
-    BookmarksService.insertBookmark(req.app.get('db'), newBookmark)
+    BookmarksService.insertBookmark(req.app.get("db"), newBookmark)
       .then(bookmark => {
         res
           .status(201)
@@ -55,34 +59,32 @@ bookmarkRouter
   });
 
 bookmarkRouter
-  .route('/:bookmarkId')
+  .route("/:bookmarkId")
   .all((req, res, next) => {
     const { bookmarkId } = req.params;
-    const knexInstance = req.app.get('db');
-    BookmarksService
-      .getBookmarkById(knexInstance, bookmarkId)
+    const knexInstance = req.app.get("db");
+    BookmarksService.getBookmarkById(knexInstance, bookmarkId)
       .then(bookmark => {
         if (!bookmark) {
           // logger.error(`Bookmark with id ${bookmarkId} not found.`);
           return res
             .status(404)
-            .json({ error: { message: 'Bookmark Not Found' } });
+            .json({ error: { message: "Bookmark Not Found" } });
         }
-        res.bookmark = bookmark
-        next()
+        res.bookmark = bookmark;
+        next();
       })
       .catch(next);
   })
   .get((req, res, next) => {
-    res.json(serializeBookmark(res.bookmark))
+    res.json(serializeBookmark(res.bookmark));
   })
   .delete((req, res) => {
-
     const bookmarkIndex = bookmarks.findIndex(b => b.id == id);
 
     if (bookmarkIndex === -1) {
       logger.error(`Bookmark with id ${bookmarkId} not found.`);
-      return res.status(404).send('Not found');
+      return res.status(404).send("Not found");
     }
 
     bookmarks.splice(bookmarkIndex, 1);
@@ -91,30 +93,27 @@ bookmarkRouter
     res.status(204).end();
   })
   .patch(bodyParser, (req, res, next) => {
-    const { title, url, description, rating } = req.body
-    const bookmarkToUpdate = { title, url, description, rating }
-    console.log(bookmarkToUpdate)
-    const numberOfValues = Object.values(bookmarkToUpdate).filter(Boolean).length
+    const { title, url, description, rating } = req.body;
+    const bookmarkToUpdate = { title, url, description, rating };
+    console.log(bookmarkToUpdate);
+    const numberOfValues = Object.values(bookmarkToUpdate).filter(Boolean)
+      .length;
     if (numberOfValues === 0) {
-      return res
-        .status(400)
-        .json({
-          error: {
-            message: `Request body must contain either 'title', 'url', 'description' or 'rating'.`
-          }
-        })
+      return res.status(400).json({
+        error: {
+          message: `Request body must contain either 'title', 'url', 'description' or 'rating'.`
+        }
+      });
     }
     BookmarksService.updateBookmark(
-      req.app.get('db'),
-      bookmarkId,
+      req.app.get("db"),
+      req.params.bookmarkId,
       bookmarkToUpdate
     )
       .then(numRowsAffected => {
-        res
-          .status(204)
-          .end()
+        res.status(204).end();
       })
-      .catch(next)
-  })
+      .catch(next);
+  });
 
 module.exports = bookmarkRouter;
--- test/bookmarks-endpoints.spec.js
+++ test/bookmarks-endpoints.spec.js
@@ -1,8 +1,11 @@
 const knex = require("knex");
 const app = require("../src/app");
-const { makeBookmarksArray, makeMaliciousBookmark } = require('./bookmarks.fixtures')
+const {
+  makeBookmarksArray,
+  makeMaliciousBookmark
+} = require("./bookmarks.fixtures");
 
-describe("Bookmarks Endpoints", function () {
+describe("Bookmarks Endpoints", function() {
   let db;
 
   before("make knex instance", () => {
@@ -45,7 +48,7 @@ describe("Bookmarks Endpoints", function () {
       });
     });
     context("Given there are bookmarks", () => {
-      const testBookmark = makeBookmarksArray()
+      const testBookmark = makeBookmarksArray();
       beforeEach("insert bookmarks", () => {
         return db.into("bookmarks").insert(testBookmark);
       });
@@ -59,7 +62,7 @@ describe("Bookmarks Endpoints", function () {
   });
 
   describe("POST /api/bookmarks", () => {
-    it("creates a bookmark, responding with 201 and the new bookmark", function () {
+    it("creates a bookmark, responding with 201 and the new bookmark", function() {
       const newBookmark = {
         title: "new title",
         url: "https://www.newtitle.com",
@@ -76,7 +79,7 @@ describe("Bookmarks Endpoints", function () {
           expect(res.body.url).to.eql(newBookmark.url);
           expect(res.body.description).to.eql(newBookmark.description);
           expect(res.body.rating).to.eql(newBookmark.rating); //rating does not come back
-          console.log(res.body)
+          console.log(res.body);
           expect(res.body).to.have.property("id");
           expect(res.headers.location).to.eql(`/api/bookmarks/${res.body.id}`);
         })
@@ -109,41 +112,39 @@ describe("Bookmarks Endpoints", function () {
       });
     });
 
-    it('removes XSS attack content from response', () => {
-      const { maliciousBookmark, expectedBookmark } = makeMaliciousBookmark()
+    it("removes XSS attack content from response", () => {
+      const { maliciousBookmark, expectedBookmark } = makeMaliciousBookmark();
       return supertest(app)
-        .post('/api/bookmarks')
+        .post("/api/bookmarks")
         .set("Authorization", `Bearer ${process.env.API_TOKEN}`)
         .send(maliciousBookmark)
         .expect(201)
         .expect(res => {
-          expect(res.body.title).to.eql(expectedBookmark.title)
-          expect(res.body.description).to.eql(expectedBookmark.description)
-        })
-    })
+          expect(res.body.title).to.eql(expectedBookmark.title);
+          expect(res.body.description).to.eql(expectedBookmark.description);
+        });
+    });
   });
 
-  describe.only(`PATCH /api/bookmarks/:bookmarkId`, () => {
-    const testBookmark = makeBookmarksArray()
+  describe(`PATCH /api/bookmarks/:bookmarkId`, () => {
+    const testBookmark = makeBookmarksArray();
 
-    beforeEach('insert bookmark', () => {
-      return db
-        .into('bookmarks')
-        .insert(testBookmark)
-    })
+    beforeEach("insert bookmark", () => {
+      return db.into("bookmarks").insert(testBookmark);
+    });
 
-    it('responds with 204 and updates the bookmark', () => {
-      const idToUpdate = 3
+    it("responds with 204 and updates the bookmark", () => {
+      const idToUpdate = 3;
       const updateBookmark = {
-        title: 'updated bookmark',
-        url: 'http://updatedbookmark.com',
-        description: 'updated description',
+        title: "updated bookmark",
+        url: "http://updatedbookmark.com",
+        description: "updated description",
         rating: 5
-      }
+      };
       const expectedBookmark = {
         ...testBookmark[idToUpdate - 1],
         ...updateBookmark
-      }
+      };
       return supertest(app)
         .patch(`/api/bookmarks/${idToUpdate}`)
         .set("Authorization", `Bearer ${process.env.API_TOKEN}`)
@@ -152,44 +153,47 @@ describe("Bookmarks Endpoints", function () {
         .then(res =>
           supertest(app)
             .get(`/api/bookmarks/${idToUpdate}`)
+            .set("Authorization", `Bearer ${process.env.API_TOKEN}`)
             .expect(expectedBookmark)
-        )
-    })
-    it('responds with 400 when no required fields supplied', () => {
-      const idToUpdate = 3
+        );
+    });
+    it("responds with 400 when no required fields supplied", () => {
+      const idToUpdate = 3;
       return supertest(app)
         .patch(`/api/bookmarks/${idToUpdate}`)
         .set("Authorization", `Bearer ${process.env.API_TOKEN}`)
-        .send({ irrevelantField: 'foo' })
+        .send({ irrevelantField: "foo" })
         .expect(400, {
           error: {
-            message: "Request body must contain either 'title', 'url', 'description' or 'rating'."
+            message:
+              "Request body must contain either 'title', 'url', 'description' or 'rating'."
           }
-        })
-    })
-    it('responds with 204 when updating only a subset of fields', () => {
-      const idToUpdate = 2
+        });
+    });
+    it("responds with 204 when updating only a subset of fields", () => {
+      const idToUpdate = 2;
       const updateBookmark = {
-        title: 'updated bookmark title',
-      }
+        title: "updated bookmark title"
+      };
       const expectedBookmark = {
         ...testBookmark[idToUpdate - 1],
         ...updateBookmark
-      }
+      };
 
       return supertest(app)
         .patch(`/api/bookmarks/${idToUpdate}`)
         .set("Authorization", `Bearer ${process.env.API_TOKEN}`)
         .send({
           ...updateBookmark,
-          fieldToIgnore: 'should not be in GET response'
+          fieldToIgnore: "should not be in GET response"
         })
         .expect(204)
         .then(res =>
           supertest(app)
             .get(`/api/bookmarks/${idToUpdate}`)
+            .set("Authorization", `Bearer ${process.env.API_TOKEN}`)
             .expect(expectedBookmark)
-        )
-    })
-  })
+        );
+    });
+  });
 });
